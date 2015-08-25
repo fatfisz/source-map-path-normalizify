c2c2b2c2004ffd259c4661c3f16e54764cfb3e32
fatfisz <fatfisz@gmail.com>
2015-08-25 23:27:04 +0200

    Fixed a strange bug.
    Browserify would not include files when sourceRoot was set.

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ var defaults = {
   sourceRoot: '/',
 };
 
+function transformPath(root, from, to) {
+  return root + path.relative(from, to).replace(/\\/g, '/');
+}
+
 module.exports = transformTools.makeStringTransform(
   'source-map-path-normalizify',
   function (content, transformOptions, done) {
     var options = assign({}, defaults, transformOptions.config);
+    var transformer = transformPath.bind(null, options.sourceRoot, options.root);
+
     var sourceMap = convertSourceMap.fromSource(content);
 
     if (!sourceMap) {
@@ -24,13 +30,7 @@ module.exports = transformTools.makeStringTransform(
 
     var sources = sourceMap.getProperty('sources');
 
-    sourceMap.setProperty('sources', sources.map(function (source) {
-      // The path will be relative to the __dirname.
-      // Also, replace "\" with "/" on Windows.
-      return path.relative(options.root, source).replace(/\\/g, '/');
-    }));
-
-    sourceMap.setProperty('sourceRoot', options.sourceRoot);
+    sourceMap.setProperty('sources', sources.map(transformer));
 
     // Remove the old source map and append new
     var result = convertSourceMap.removeComments(content) +
